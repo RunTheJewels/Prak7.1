f31f78c4c5944d10a2b0ba876f0dbc13345e95d5
RunTheJewels <WoodyAllenZombieJesus@yandex.ru>
2017-10-20 13:50:33 +0300

    Ya ronayu zapad u

--- src/test.cpp
+++ src/test.cpp
@@ -10,79 +10,14 @@ using namespace std;
 
 const int SIZE = 3; 
 
-const int N = 5;
+const int N = 20;
 
 SType *EV;
-
-// void mult(SMatrix **A, SMatrix **B, SMatrix **C) {
-// 	SMatrix	*u = new SMatrix(*this, nRows, nMin),
-// 		*vt = new SMatrix(*this, nMin, nCols);
-// 	SReal *S = new SReal[nMin];
-// 	SType *SC = new SType[nMin];
-//         int	*desc = getDesc(),
-// 		*descU = u->getDesc(),
-// 		*descVT = vt->getDesc();
-
-// 	SType *dataCopy = new SType[myProcSize];
-// 	memcpy(dataCopy, data, myProcSize * sizeof(SType));
-
-// 	SType *work = new SType[10 * nRows * nCols];
-
-// 	int ione = 1, info, lwork = 10 * nRows * nCols;
-
-// #ifdef USE_COMPLEX
-// 	SType *rwork = new SType[1 + 4 * max(nRows, nCols)];
-// 	int lrwork = 1 + 4 * max(nRows, nCols) * max(nRows, nCols);
-// #endif
-
-// 	pzheev_((char*) "V", (char*) "U", &nRows, dataCopy, &ione, &ione, 
-// 		desc, S, u->data, &ione, &ione, descU, work, &lwork, rwork, 
-// 		&lrwork, &info);
-	
-// #ifdef USE_COMPLEX
-// 	lwork = (int) work[0].real();
-// #else
-// 	lwork = (int) work[0];
-// #endif
-	
-// 	for (int i = 0; i < nMin; i++)
-// 	{
-// 		SC[i] = exp(S[i] * deltat * SType(0,-1));
-// 	}
-
-// 	delete[] work;
-// 	work = new SType[lwork];
-	
-// 	delete[] work, dataCopy;
-// #ifdef USE_COMPLEX
-// 	delete[] rwork;
-// #endif
-// 	delete[] desc, descU, descVT;
-
-// 	if (SSize != NULL) {
-// 		*SSize = nMin;
-// 	}
-
-// 	if (U != NULL) {
-// 		*U = u;
-// 	} else {
-// 		delete u;
-// 	}
-
-// 	if (VT != NULL) {
-// 		*VT = vt;
-// 	} else {
-// 		delete vt;
-// 	}
-
-// 	*ev = SC;
-
-// 	return S;
-// }
 
 SType fillerZero(int i, int j) {
 	return 0;
 }
+
 SType fillerEV(int i, int j) {
 	if (i == j) {
 		return EV[i];
@@ -96,59 +31,50 @@ SType filler1(int i, int j)
 		 //SType(4,-0.76), SType(-1.5,-3), SType(4,0.76), SType(-1,-1)};
 		 // SType mat[] = {SType(1,0), SType(2,0), SType(3,0), SType(2,0), SType(-5,0),
 		 // SType(4,0), SType(3,0), SType(4,0), SType(0,0)};
-	SType mat[] = {SType(2,0), SType(0,0), SType(0,0), SType(0,0), SType(1,0),
-		 SType(3,0), SType(0,0), SType(3,0), SType(0,0)};
+	
+	SType mat[] = {SType(2,0), SType(0,0), SType(0,0),
+		 		   SType(0,0), SType(1,0), SType(3,0), 
+		 		   SType(0,0), SType(3,0), SType(0,0)};
+	
 	return mat[i*3+j];
 }
 
 SType fillerRo(int i, int j) {
 	if (i == 1 and j == 1) return 1;
+	//if (i==0 or i ==2 or j==0 or j==2) return 0.5;
 	return 0;
 }
 
 /// SVD demo
-void printSVD(SMatrix &x, bool isRoot) {
+void printSVD(SMatrix &x, bool isRoot, int rank) {
+	
 	int SSize;
+	
 	SMatrix *U, *VT;
 	SReal *S = x.calculateSVD(&SSize, &U, &VT, isRoot, &EV);
+	
 	if (isRoot) {
 		cout << endl;
 		cout << "Матрица собственных векторов:" << endl;
 		cout << " =" << endl;
 	}
+	
 	cout << *U;
+	
 	SMatrix eigenM(*U,SIZE,SIZE);
-	// eigenM.setIdentity();
 	eigenM.populate(&fillerEV);
+	
 	if (isRoot) {
 		cout << endl;
 		cout << "Матрица собственных значений:" << endl;
 		cout << " =" << endl;
 	}
+	
 	cout << eigenM;
-	if (isRoot) {
-		// cout << " *" << endl;
-		// cout << "  diag([";
-		// for (int i = 0; i < SSize; i++) {
-		// 	if (i > 0) {
-		// 		cout << " ";
-		// 	}
-		// 	cout << setprecision(8) << S[i];
-		// }
-		// cout << "])" << endl;
-		// cout << " *" << endl;
-		// cout << EV << endl;
-		// for (int i = 0; i < SSize; i++)
-		// {
-		// 	cout << EV[i] << " ";
-		// }
-		// cout << endl;
-	}
+
 	if (isRoot) cout << endl;
-	// cout << *VT;
 	
 	SMatrix res(*U,SIZE,SIZE);
-	// res.setIdentity();
 	res.populate(&fillerZero);
 
 	int size = SIZE, intone = 1;
@@ -164,7 +90,6 @@ void printSVD(SMatrix &x, bool isRoot) {
 	pzgemm_((char *) "N", (char *) "C", &size, &size, &size, &one, res.data, &intone, &intone, descc, U->data, &intone, &intone, desca,
 		&zero, eigenM.data, &intone, &intone, descb);
 
-	// cout << eigenM;
 
 	SMatrix Ro(*U,SIZE,SIZE);
 	int *descRo = Ro.getDesc();
@@ -174,22 +99,57 @@ void printSVD(SMatrix &x, bool isRoot) {
 
 	for (int i = 0; i < N; i++)
 	{
+		Ro.barrier();
+		res.barrier();
+		eigenM.barrier();
+		MPI_Barrier(MPI_COMM_WORLD);
+		
 		res.populate(&fillerZero);
-		if (isRoot)	cout << "iter = " << i+1 << ":\n";
+		
 		pzgemm_((char *) "N", (char *) "N", &size, &size, &size, &one, eigenM.data, &intone, &intone, descb, Ro.data, 
 		&intone, &intone, descRo, &zero, res.data, &intone, &intone, descc);
+		
+		Ro.barrier();
+		res.barrier();
+		eigenM.barrier();
+		MPI_Barrier(MPI_COMM_WORLD);
+		
 		pzgemm_((char *) "N", (char *) "C", &size, &size, &size, &one, res.data, &intone, &intone, descc, eigenM.data, 
 		&intone, &intone, descb, &zero, Ro.data, &intone, &intone, descRo);
+		
+		if (isRoot)	cout << "iter = " << i+1 << ":\n";
+		
+		
+		Ro.barrier();
+		res.barrier();
+		eigenM.barrier();
+		MPI_Barrier(MPI_COMM_WORLD);
+		
 		cout << Ro;
+		
+		Ro.barrier();
+		res.barrier();
+		eigenM.barrier();
+		
+		MPI_Barrier(MPI_COMM_WORLD);
 	}
-
+	
+		cout << rank << endl;
+	
+		Ro.barrier();
+		res.barrier();
+		eigenM.barrier();
+		MPI_Barrier(MPI_COMM_WORLD);
+	
 	delete U, VT, SSize; 
 	delete[] S;
 }
 
 int main(int argc, char **argv) {
+	
 	MPI_Init(&argc, &argv);
 	int size,rank;
+	
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
@@ -200,13 +160,9 @@ int main(int argc, char **argv) {
 		x.setIdentity();
 		x.populate(&filler1);
 		cout << x;
-		printSVD(x, isRoot);
+		printSVD(x, isRoot,rank);
 	}
-
-	#ifdef USE_COMPLEX
-	//SType we[] ={SType(2,1)};
-	//cout << we[0];
-	#endif
+	
 	SMatrix::exit();
 	return 0;
 }
